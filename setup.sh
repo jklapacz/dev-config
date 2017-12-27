ee3a3cc5aaf178775156d80f7411446b965dd4bd
Jakub Klapacz <jklapacz@pindrop.com>
2017-12-26 22:12:21 -0600

    Adding tmux setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 CONFIG_DIR=$(pwd)
 VIMRC=$CONFIG_DIR/vimrc
 VIM_DIR=$CONFIG_DIR/vim
+TMUX_DIR=$CONFIG_DIR/tmux
+TMUX_CONF=$TMUX_DIR/tmux.conf
+TMUX_CONF_LOCAL=$TMUX_DIR/tmux.conf.local
 DEPS=$CONFIG_DIR/dependencies
 
 cd ~
@@ -12,6 +15,10 @@ cp .vim .vim.old
 
 ln -s $VIMRC .vimrc
 ln -s $VIM_DIR .vim
+
+ln -s $TMUX_CONF .tmux.conf
+ln -s $TMUX_CONF_LOCAL .tmux.conf.local
+
 
 mkdir $DEPS 
 cd $DEPS 
