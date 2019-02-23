91fdafece194834565fc9b7363a2da053c1de35d
Jakub Klapacz <jklapacz@pindrop.com>
2019-02-23 16:08:28 -0500

    Simplifying configs

--- setup.sh
+++ setup.sh
@@ -7,25 +7,23 @@ TMUX_DIR=$CONFIG_DIR/tmux
 TMUX_CONF=$TMUX_DIR/tmux.conf
 TMUX_CONF_LOCAL=$TMUX_DIR/tmux.conf.local
 DEPS=$CONFIG_DIR/dependencies
+timestamp=$(date -u "+%Y%m%d%H%M%S")
 
-cd ~
+pushd $HOME
+    mv .vimrc .vimrc.backup_$timestamp
+    mv .vim .vim.backup_$timestamp
+    
+    ln -s $VIMRC .vimrc
+    ln -s $VIM_DIR .vim
+    
+    #ln -s $TMUX_CONF .tmux.conf
+    #ln -s $TMUX_CONF_LOCAL .tmux.conf.local
+popd
 
-cp .vimrc .vimrc.old
-cp .vim .vim.old
+# Add config if not present
+if ! grep -Fxq "$(cat .gitconfig)" ~/.gitconfig; then
+    cat .gitconfig >> ~/.gitconfig
+fi
 
-ln -s $VIMRC .vimrc
-ln -s $VIM_DIR .vim
-
-ln -s $TMUX_CONF .tmux.conf
-ln -s $TMUX_CONF_LOCAL .tmux.conf.local
-
-
-mkdir $DEPS 
-cd $DEPS 
-
-git clone https://github.com/VundleVim/Vundle.vim $VIM_DIR/bundle/Vundle.vim
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-
-rm $VIM_DIR/vim
+./restore_configs.sh $VIM_DIR
+./setup_zsh.sh
