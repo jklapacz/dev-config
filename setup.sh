71f88c4fb0424007f8ba01cf7d5f0c70595f4722
Jakub Klapacz <jklapacz@pindrop.com>
2019-02-23 16:18:24 -0500

    include aliases

--- setup.sh
+++ setup.sh
@@ -8,14 +8,17 @@ TMUX_CONF=$TMUX_DIR/tmux.conf
 TMUX_CONF_LOCAL=$TMUX_DIR/tmux.conf.local
 DEPS=$CONFIG_DIR/dependencies
 timestamp=$(date -u "+%Y%m%d%H%M%S")
+aliases=$CONFIG_DIR/.aliases
 
 pushd $HOME
     mv .vimrc .vimrc.backup_$timestamp
     mv .vim .vim.backup_$timestamp
+    mv .aliases .aliases.backup_$timestamp
     
     ln -s $VIMRC .vimrc
     ln -s $VIM_DIR .vim
     
+    ln -s $aliases .aliases
     #ln -s $TMUX_CONF .tmux.conf
     #ln -s $TMUX_CONF_LOCAL .tmux.conf.local
 popd
